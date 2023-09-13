49be77af5d44a70f9c103fdc130157fadcc7a770
Miguel-SLO <miguel.santos@etml-es.ch>
2023-09-13 09:20:54 +0200

    Projet MPLAB supprimé !
    Plusieurs erreurs de builds et de configuration. Projet recommencé à zero

--- soft/2312_BadgePlace_PIC32/firmware/2312_BadgePlace_PIC32.X/build/2312_Config/production/_ext/593738990/helpers.o.d
+++
@@ -1,5 +0,0 @@
-build/2312_Config/production/_ext/593738990/helpers.o: \
- ../../../../../../framework/tcpip/src/common/helpers.c \
- ../../../../../../framework/tcpip/src/common/helpers.h
-
-../../../../../../framework/tcpip/src/common/helpers.h:

e11b27f97871b144ac6a90e67ebf02165ef402fb
Miguel-SLO <miguel.santos@etml-es.ch>
2023-09-19 09:18:54 +0200

    Suppression tests PIC32

--- soft/2312_BadgePlace_PIC32/firmware/2312_BadgePlace_PIC32.X/build/2312_Config/debug/_ext/1833714580/hash_fnv.o.d
+++
@@ -1,5 +0,0 @@
-build/2312_Config/debug/_ext/1833714580/hash_fnv.o: \
- ../../../../../../framework/tcpip/src/hash_fnv.c \
- ../../../../../../framework/tcpip/src/hash_fnv.h
-
-../../../../../../framework/tcpip/src/hash_fnv.h:
--- soft/2312_BadgePlace_PIC32/firmware/2312_BadgePlace_PIC32.X/build/2312_Config/debug/_ext/1833714580/oahash.o.d
+++
@@ -1,5 +0,0 @@
-build/2312_Config/debug/_ext/1833714580/oahash.o: \
- ../../../../../../framework/tcpip/src/oahash.c \
- ../../../../../../framework/tcpip/src/oahash.h
-
-../../../../../../framework/tcpip/src/oahash.h:
